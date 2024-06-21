4b06b42860c2a8a8c856d02eccddc16805b87b67
brendan1928 <73602969+brendan1928@users.noreply.github.com>
2024-06-20 20:34:39 -0400

    Finalized treemap!

--- Static/js/treeMap.js
+++ Static/js/treeMap.js
@@ -6,12 +6,22 @@ function createTreeMap() {
         let asthmaData = data.filter(item => item["Health Condition"] === "Current Asthma");
         let obesityData = data.filter(item => item["Health Condition"] === "Obesity");
         let depressionData = data.filter(item => item["Health Condition"] === "Depression");
+        let firstTrace = data.filter(item => item["Health Condition"] === "Current Asthma");
 
-        asthmaTrace = {
+        firstTrace = { 
+            type: 'treemap',
+            labels: firstTrace.map(item => item["State"]),
+            parents: firstTrace.map(item => item["Health Condition"]),
+            values: firstTrace.map(item => Math.round(item["Condition Prevalence (%)"] * item["Population"])),
+            hovertemplate: '<b>%{label}</b><br>%{value}',
+            name: 'Asthma'
+        };
+
+        asthmaTrace = { 
             type: 'treemap',
             labels: asthmaData.map(item => item["State"]),
             parents: asthmaData.map(item => item["Health Condition"]),
-            values: asthmaData.map(item => Math.round(item["Condition Prevalence (%)"]*item["Population"])),
+            values: asthmaData.map(item => Math.round(item["Condition Prevalence (%)"] * item["Population"])),
             hovertemplate: '<b>%{label}</b><br>%{value}',
             name: 'Asthma'
         };
@@ -20,7 +30,7 @@ function createTreeMap() {
             type: 'treemap',
             labels: obesityData.map(item => item["State"]),
             parents: obesityData.map(item => item["Health Condition"]), 
-            values: obesityData.map(item => Math.round(item["Condition Prevalence (%)"]*item["Population"])),
+            values: obesityData.map(item => Math.round(item["Condition Prevalence (%)"] * item["Population"])),
             hovertemplate: '<b>%{label}</b><br>%{value}',
             name: 'Obesity'
         };
@@ -29,43 +39,52 @@ function createTreeMap() {
             type: 'treemap',
             labels: depressionData.map(item => item["State"]),
             parents: depressionData.map(item => item["Health Condition"]),
-            values: depressionData.map(item => Math.round(item["Condition Prevalence (%)"]*item["Population"])),
+            values: depressionData.map(item => Math.round(item["Condition Prevalence (%)"] * item["Population"])),
             hovertemplate: '<b>%{label}</b><br>%{value}',
             name: 'Depression'
         };
 
+        var frames = [{
+            name: 'Asthma',
+            data: [asthmaTrace]
+          },
+          {
+            name: 'Obesity',
+            data: [obesityTrace]
+          },
+          {
+            name: 'Depression',
+            data: [depressionTrace]
+          }
+        ];
+
         layout = {
             title: 'States with Highest Health Condition Prevalence',
             updatemenus: [{
-                buttons: [
-                    { label: 'Asthma', method: updateChartTree, args: ['Asthma'] },
-                    { label: 'Obesity', method: updateChartTree, args: ['Obesity'] },
-                    { label: 'Depression', method: updateChartTree, args: ['Depression'] }
-                ],
+                buttons: [{
+                    method: 'animate',
+                    args: [['Asthma']],
+                    label: 'Asthma'
+                },
+                {
+                    method: 'animate',
+                    args: [['Obesity']],
+                    label: 'Obesity'
+                },
+                {
+                    method: 'animate',
+                    args: [['Depression']],
+                    label: 'Depression'
+                }],
                 direction: 'down',
                 showactive: true,
+                type: 'buttons'
             }]
         };
 
-
-
-        Plotly.newPlot('treeMap', [asthmaTrace], layout);
+        // Create the initial plot with asthmaTrace
+        Plotly.newPlot('treeMap', [firstTrace], layout).then(function(){
+            Plotly.addFrames('treeMap', frames);
+        });
     });
-}
-
-function updateChartTree(selectedCondition) {
-    switch (selectedCondition) {
-        case 'Asthma':
-            Plotly.react('treeMap', [asthmaTrace], layout);
-            break;
-        case 'Obesity':
-            Plotly.react('treeMap', [obesityTrace], layout);
-            break;
-        case 'Depression':
-            Plotly.react('treeMap', [depressionTrace], layout);
-            break;
-        default:
-            Plotly.react('treeMap', [asthmaTrace], layout);
-            break;
-    }
 }
